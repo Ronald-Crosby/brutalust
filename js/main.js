499b7d62e9e1e529a9907b073c403c7725bdaf05
Olly Robinson <ollythedeveloper@gmail.com>
2020-04-14 17:17:23 +0100

    start project

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
-console.log('hello world')
+const headerTag = document.querySelectorAll("h1");
+const h2Tag = document.querySelectorAll("h2");
+
+const runRandom = tag => {
+	const originalContent = tag.innerHTML
+	let newContent = ''
+
+	let num = 0
+
+	const writeWord = setInterval(() => {
+		num++
+		newContent = originalContent.substring(0, num)
+		if (originalContent == tag.innerHTML) {
+			clearInterval(writeWord)
+		}
+	}, 100)
+
+	const generateRandomCharacters = setInterval(() => {
+		tag.innerHTML = newContent
+
+		for (i = newContent.length; i < originalContent.length; i++) {
+			tag.innerHTML = tag.innerHTML + '.'
+		}
+
+		if (tag.innerHTML == originalContent) {
+			clearInterval(generateRandomCharacters)
+		}
+	}, 50)
+}
+
+headerTag.forEach((tag) => {
+  runRandom(tag)
+});
